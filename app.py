2fd39949f19dd5c20b854a9b3d55175cca3d7fc6
Will Torres <52608541+torrwill@users.noreply.github.com>
2023-11-01 16:45:06 -0400

    Upload EDA application files

---
+++ app.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+# Web App Title
+st.markdown('''
+# **The EDA App**
+This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
+---
+''')
+
+# Upload CSV data
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    example_csv_url = "https://raw.githubusercontent.com/torrwill/data/master/car_price_assignment.csv"
+    st.sidebar.markdown(f"[Example CSV input file]({example_csv_url})")
+
+# Pandas Profiling Report
+@st.cache_data
+def read_uploaded_csv():
+    csv = pd.read_csv(uploaded_file)
+    return csv
+
+if uploaded_file is not None:
+    df = read_uploaded_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Pandas Profiling Report**')
+    st_profile_report(pr)
+else:
+    st.info('Awaiting for CSV file to be uploaded.')
+    button_label = 'Press to Reload Example Dataset' if st.button('Press to Reload Example Dataset') else 'Press to use Example Dataset'
+    if button_label:
+        @st.cache_data
+        def load_data():
+            a = pd.DataFrame(
+                np.random.rand(100, 5),
+                columns=['a', 'b', 'c', 'd', 'e']
+            )
+            return a
+
+        df = load_data()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
+
+# User Instructions
+st.sidebar.markdown('''
+### Instructions
+- Upload a CSV file for exploratory data analysis.
+- For example data, use the provided [Example CSV input file]({example_csv_url}).
+''')
